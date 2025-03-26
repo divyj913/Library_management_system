564223edca4440b4fddd46b0320199888e19514a
DJ <DIVYJ913@GMAIL.COM>
2025-03-26 10:37:36 +0530

    Delete interpreter.py

--- interpreter.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox, simpledialog    # Add this import
-root = tk.Tk()
-root.title(" Advanced Library Management System")
-firstlabel = tk.Label(root, text="Welcome to the Advanced Library Management System", font=("Arial", 20))
-firstlabel.pack(pady=20)
-root.geometry("1920x1080")
-root.mainloop()
